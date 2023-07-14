a91407dc7c2adcd9baeba114fdee149d51cc037c
Alex Gabor <g.c.alex998@gmail.com>
2023-07-14 16:36:30 +0300

    Added Hello World

---
+++ client/src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Ticket from './Ticket.js'
+
+function App(){
+    return(
+        <Ticket/>
+    )
+}
+
+export default App;
---
+++ client/src/components/Ticket.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Ticket(){
+    return(
+        <div className=".container">
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default Ticket;
