7907642c7eb33413a9eaac02566a94030369e8dc
Alex Gabor <g.c.alex998@gmail.com>
2023-07-14 16:49:45 +0300

    Added bootstrap classes for the Ticket component

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function Ticket(){
     return(
-        <div className=".container">
-            <p>Hello</p>
+        <div className="container">
+           <div className="row">
+                <div className="col-12">
+                    <p>This is the header</p>
+                </div>
+           </div>
         </div>
     )
 }
