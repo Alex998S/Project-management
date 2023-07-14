ab1c4cc7645c0e3fa71c5866e56b422ac2756aa0
Alex Gabor <g.c.alex998@gmail.com>
2023-07-14 17:10:15 +0300

    Imported bootstrap to Ticket.js

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,8 +1,10 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap/dist/js/bootstrap.bundle'
 
 function Ticket(){
     return(
-        <div className="container">
+        <div className="container w-25 shadow-lg">
            <div className="row">
                 <div className="col-12">
                     <p>This is the header</p>
