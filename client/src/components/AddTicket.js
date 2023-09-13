efce510fa9c9927581ff42223229d7760bdb0d62
Alex Gabor <g.c.alex998@gmail.com>
2023-09-13 17:24:53 +0300

    Added button that shows the form to add tickets

--- client/src/components/AddTicket.js
+++ client/src/components/AddTicket.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useState, userState} from "react";
 import 'bootstrap/dist/css/bootstrap.css'
 //import 'bootstrap/dist/js/bootstrap.bundle'
 import '../stylesheets/ticket.sass'
 
 
-function AddTicket(){
+function AddTicket(props){
 
-    return(
-        <div style={{display:'block',position: 'absolute'}}>Show ME</div>
-    );
+    let[show, setShow] = useState(props.props)
+
+    setShow = props.props
+
+    console.log("add ticket console", props.props)
+
+    if(setShow){
+        return(
+            <div style={{display:'block',position: 'absolute', margin: '300px'}}>Show ME</div>
+        );
+    }else{
+        return(
+            <div style={{display:'block',position: 'absolute', margin: '300px'}}>Not visible</div>
+        )
+    }
 
 }
 
--- client/src/components/LeftSideMenu.js
+++ client/src/components/LeftSideMenu.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {setState, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css'
 //import 'bootstrap/dist/js/bootstrap.bundle'
 import '../stylesheets/ticket.sass'
 import AddTicket from "./AddTicket";
 
+
+let showPopUp = false;
+
 function LeftSideMenu(){
-    return(
-        <div className="container col-2 border">
-            <button type="button" class="btn btn-primary">Create Ticket</button>
-            <AddTicket/>
-        </div>
-    )
+
+    const[pressed, setPressed] = useState(showPopUp)
+
+    if(!pressed){
+        return(
+            <div className="container col-2 border">
+                <button type="button" className="btn btn-primary" onClick={()=>setPressed(true)}>Create Ticket</button>
+                <AddTicket props={pressed}/>
+            </div>
+        )
+    }else{
+        return(
+            <div className="container col-2 border">
+                <button type="button" className="btn btn-primary" onClick={()=>setPressed(false)}>Close Ticket</button>
+                <AddTicket props={pressed}/>
+            </div>
+        )
+    }
 }
 
 export default LeftSideMenu
