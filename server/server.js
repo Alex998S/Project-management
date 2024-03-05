64669700e43f3110a036a3b1bbff253e18fc1941
Alex Gabor <g.c.alex998@gmail.com>
2024-03-05 08:47:52 +0200

    JWT token WIP

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import mongoose from "mongoose";
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import jwt from 'jsonwebtoken'
 
 const databaseURL = process.env.DATABASE
 
@@ -40,7 +41,28 @@ app.use('/tickets', ticketRouter)
 
 app.use('/', companyRouter)
 
-app.use('/users', usersRouter);
+app.use('/users', authenticateToken, usersRouter);
+
+function generateAccessToken(username) {
+    return jwt.sign(username, process.env.TOKEN_PASS, { expiresIn: '1800s' });
+}
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      console.log(err)
+  
+      if (err) return res.sendStatus(403)
+  
+      req.user = user
+  
+      next()
+    })
+  }
 
 app.listen(PORT, () => {
   connect()
