5ba358cec390e74373091803692f2f197391e760
Alex Gabor <g.c.alex998@gmail.com>
2024-09-18 17:20:58 +0300

    WIP - front end, added search for dropdowns

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -3,52 +3,77 @@ import 'bootstrap/dist/css/bootstrap.css'
 //import 'bootstrap/dist/js/bootstrap.bundle'
 import '../stylesheets/main.sass'
 
-function displayOptions(props, showList, setDropdownValue, setShowList){
+
+function displayOptions(props, showList, setDropdownValue, setShowList, searchKey, handleChange){
+
+    let defaultOptions = []
+
+    let options = []
+
+    let filteredOptions = []
+
     if(props.data.dynamic == true){
 
         let dynamicArrayName = props.data.options
 
-        let options = props.dynamicFields[dynamicArrayName]
-
-        console.log("[Dropdown]==options", options)
-        return(
-            <ul className={"dropdown-menu" + (showList ? ' show' : '')} >
-                    {options.map(element =>{
-                        if(Object.hasOwn(element, '_id')){
-                            console.log("with ID", element)
-                            return(
-                                <li><button type="button" key={element._id} onClick={()=>{setDropdownValue(`${element.first_name} ${element.last_name}`); setShowList(false)}} className="dropdown-item">{`${element.first_name} ${element.last_name}`}</button></li>
-                            )
-                        }else{
-                            console.log("without ID", element)
-                            return(
-                                <li><button type="button" key={element} onClick={()=>{setDropdownValue(element); setShowList(false)}} className="dropdown-item">{element}</button></li>
-                            )
-                        }
-                        
-                    })}
-            </ul>
-        )
+        defaultOptions = props.dynamicFields[dynamicArrayName]
     }else{
-        return(
-            <ul className={"dropdown-menu" + (showList ? ' show' : '')} >
-                {props.data.options.map(element =>{
-                    return(
-                        <li><button type="button" key={element} onClick={()=>{setDropdownValue(element); setShowList(false)}} className="dropdown-item">{element}</button></li>
-                    )
-                })}
-            </ul>
-        )
-        
+        defaultOptions = props.data.options
     }
+
+    if(Object.hasOwn(defaultOptions[0], '_id')){
+        defaultOptions.map(element=>{
+            element.displayName = `${element.first_name} ${element.last_name}`
+            options.push(element)
+        })
+    }else{
+        defaultOptions.map(element=>{
+            let object = {
+                displayName: element
+            }
+            options.push(object)
+        })
+    }
+
+    console.log("options in dropdown", options)
+
+    if(searchKey != ""){
+        options.map(element=>{
+            let elementValue = element.displayName
+            let valueToLowerCase = elementValue.toLowerCase()
+            if(valueToLowerCase.includes(searchKey.toLowerCase())){
+                filteredOptions.push(element)
+            }
+        })
+    }else{
+        filteredOptions = options
+    }
+
+    console.log("in dropdown, filteredOptions", filteredOptions)
+
+    return(
+        <ul className={"dropdown-menu" + (showList ? ' show' : '')} >
+            <input type="text" className="dropdown-search" placeholder="Search..." value={searchKey} onChange={handleChange}></input>
+            {filteredOptions.map(element =>{
+                return(
+                    <li><button type="button" key={element.displayName} onClick={()=>{setDropdownValue(element.displayName); setShowList(false)}} className="dropdown-item">{element.displayName}</button></li>
+                )
+            })}
+        </ul>
+    )
+    
 }
+
 
 function Dropdown(props){
 
     const[showList, setShowList] = useState(false)
     const[dropdownValue, setDropdownValue] = useState(props.value)
+    const[searchKey, setSearchKey] = useState("")
 
-    console.log("[Dropdown]==props", props)
+    const handleChange = (e) =>{
+        setSearchKey(e.target.value)
+    }
 
     return(
         <div className="mb-3">
@@ -60,7 +85,7 @@ function Dropdown(props){
                 <button name={props.name} key={props.name} onClick={()=>setShowList(dropdownVisible(showList))} className="btn btn-primary dropdown-toggle show my-dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     {dropdownValue}
                 </button>
-                {displayOptions(props, showList, setDropdownValue, setShowList)}
+                {displayOptions(props, showList, setDropdownValue, setShowList, searchKey, handleChange)}
             </div>
         </div>
     )
@@ -76,3 +101,17 @@ function dropdownVisible(parm){
 
 export default Dropdown
 
+
+{/* <div class="dropdown">
+  <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+  <div id="myDropdown" class="dropdown-content">
+    <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+    <a href="#about">About</a>
+    <a href="#base">Base</a>
+    <a href="#blog">Blog</a>
+    <a href="#contact">Contact</a>
+    <a href="#custom">Custom</a>
+    <a href="#support">Support</a>
+    <a href="#tools">Tools</a>
+  </div>
+</div> */}
